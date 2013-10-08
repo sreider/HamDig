f04f858ca8ef2e0fead71bd0a64de49687270c22
Sampson Reider <sreider@burke001-150-209-200-100.hamilton.edu>
2013-10-08 11:02:07 -0400

    Initial commit

---
+++ HamDig/HDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HDAppDelegate.h
+//  HamDig
+//
+//  Created by Sampson Reider on 10/6/13.
+//  Copyright (c) 2013 Hamilton College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
